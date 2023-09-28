fa0517090771f40b895c63aa15c591569aae896f
giuseppesciacca <sciaccagi@gmail.com>
2023-09-28 17:29:18 +0200

    ADD milestone 4 bonus

--- src/main/java/org/java/lessons/Main.java
+++ src/main/java/org/java/lessons/Main.java
@@ -14,13 +14,22 @@ public class Main {
 		final String password = "root";
 
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Inserisci lo stato che vuoi filtrare: ");
+		System.out.print("Search: ");
 		String filt = sc.nextLine();
-		sc.close();
 
 		try (Connection con = DriverManager.getConnection(url, user, password)) {
 			System.out.println("\nConnessione stabilita correttamente\n");
-			getCountry(con, filt);
+			getCountryWithFilter(con, filt);
+
+			System.out.print("Choose a country id: ");
+			String idString = sc.nextLine();
+			int idCountry = Integer.parseInt(idString);
+
+			getLanguagesOfACountry(con, idCountry);
+
+			getStatsAboutACountry(con, idCountry);
+
+			sc.close();
 		} catch (Exception e) {
 
 			System.out.println("Errore di connessione: " + e.getMessage());
@@ -28,16 +37,13 @@ public class Main {
 
 		System.out.println("\n----------------------------------\n");
 		System.out.println("The end");
-
 	}
 
-	private static final void getCountry(Connection con, String filter) {
+	private static final void getCountryWithFilter(Connection con, String filter) {
 
 		final String sql = "SELECT c.name AS country_name, r.name AS region_name, r.region_id, c2.name AS continent_name\r\n"
-				+ "FROM countries c \r\n"
-				+ "LEFT JOIN regions r ON r.region_id  = c.region_id \r\n"
-				+ "LEFT JOIN continents c2 ON c2.continent_id = r.continent_id \r\n"
-				+ "WHERE c.name LIKE ?";
+				+ "FROM countries c \r\n" + "LEFT JOIN regions r ON r.region_id  = c.region_id \r\n"
+				+ "LEFT JOIN continents c2 ON c2.continent_id = r.continent_id \r\n" + "WHERE c.name LIKE ?;";
 
 		try {
 
@@ -62,4 +68,67 @@ public class Main {
 		}
 	}
 
+	private static final void getLanguagesOfACountry(Connection con, int idCountry) {
+
+		final String sql = "SELECT c.name AS country_name, l.`language` \r\n" + "FROM countries c \r\n"
+				+ "LEFT JOIN country_languages cl ON cl.country_id = c.country_id\r\n"
+				+ "LEFT JOIN languages l ON l.language_id = cl.language_id  \r\n" + "WHERE c.country_id = ?;";
+
+		try {
+
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setInt(1, idCountry);
+
+			ResultSet rs = ps.executeQuery();
+
+			String country = "";
+			String languages = "";
+
+			while (rs.next()) {
+
+				String language = rs.getString("language");
+				languages += language + ", ";
+				
+				String countryName = rs.getString("country_name");
+				country = countryName;
+			}
+			System.out.println("\nDetails for country: " + country);
+			System.out.println("Languages: " + languages);
+
+		} catch (Exception e) {
+
+			System.out.println(e.getMessage());
+		}
+	}
+
+	private static final void getStatsAboutACountry(Connection con, int idCountry) {
+
+		final String sql = "SELECT c.name AS name_country, cs.population, cs.`year` AS year_stat, cs.gdp \r\n"
+				+ "FROM countries c \r\n" + "LEFT JOIN country_stats cs ON cs.country_id = c.country_id \r\n"
+				+ "WHERE c.country_id = ? \r\n" + "ORDER BY YEAR DESC\r\n" + "LIMIT 1;";
+
+		try {
+
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setInt(1, idCountry);
+
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+
+				String population = rs.getString("population");
+				int yearStat = rs.getInt("year_stat");
+				long gdp = rs.getLong("gdp");
+
+				System.out.println("Most recent stats");
+				System.out.println("Year: " + yearStat);
+				System.out.println("Population: " + population);
+				System.out.println("GDP: " + gdp + "\n");
+			}
+
+		} catch (Exception e) {
+
+			System.out.println(e.getMessage());
+		}
+	}
 }
